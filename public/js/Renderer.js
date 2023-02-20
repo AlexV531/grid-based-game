884fa0c6041f96bc383379bdb8909482737bfb44
AlexV531 <v.alexander.531@gmail.com>
2023-02-20 16:41:32 -0500

    Added game objects to this rendering style.

--- public/js/Renderer.js
+++ public/js/Renderer.js
@@ -27,8 +27,6 @@ export default class Renderer {
 	// The renderer will render the tilemap and all the gameobjects in a specified level.
 	render() {
 
-		// TILE MAP //
-
 		// Render the tile map starting at the top and ending at the bottom.
 		// That way elements in front of other elements will be drawn correctly
 		this.context.fillStyle = '#FFFFFF'
@@ -39,28 +37,26 @@ export default class Renderer {
 		let yEnd = Math.floor((-pos.y / (this.canvas.width / SCALE)) / width)
 		let xStart = Math.floor((-pos.x / (this.canvas.width / SCALE)) / width)
 		let xEnd = Math.floor(((-pos.x + this.canvas.width) / (this.canvas.width / SCALE)) / width)
+
+		// Loop through each row that is visible on the screen
 		for(let y = yStart; y >= yEnd; y--) {
+			// TILE MAP //
+			// Loop through each tile in the row
 			for(let x = xStart; x <= xEnd; x++) {
 				// Each tile has an image index, which is used to find the asset needed to render the tile
 				if(this.tileMap.tileList[x] != null && this.tileMap.tileList[x][y]) {
 					this.context.drawImage(this.assets[0][this.tileMap.tileList[x][y].getImageIndex()], x*width, y*width, width, width)
 				}
 			}
-		}
-		
-		/*
-		// Render everything, not just in camera:
-		for(let y = 0; y < this.tileMap.size; y++) {
-			for(let x = 0; x < this.tileMap.size; x++) {
-				// Each tile has an image index, which is used to find the asset needed to render the tile
-				this.context.drawImage(this.assets[0][this.tileMap.tileList[x][y].getImageIndex()], x*width, y*width, width, width)
+			// GAME OBJECTS //
+			// Loop through each game object, check if it is on the current row
+			// If so, render it
+			for(let i = 0; i < this.gameObjects.length; i++) {
+				if(Math.floor(this.gameObjects[i].x / width)  == y) {
+					let spriteIndex = this.gameObjects[i].getComponent(SpriteComponent).sprite
+					this.context.drawImage(this.assets[spriteIndex], this.gameObjects[i].x, this.gameObjects[i].y, width, width)
+				}
 			}
-		}
-		*/
-
-		for(let i = 0; i < this.gameObjects.length; i++) {
-			let spriteIndex = this.gameObjects[i].getComponent(SpriteComponent).sprite
-			this.context.drawImage(this.assets[spriteIndex], this.gameObjects[i].x, this.gameObjects[i].y, width, width)
 		}
 	}
 
